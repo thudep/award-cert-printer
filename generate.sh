498aeb172a090737aca5487ce011b6dc4e78bd4b
Jerry <57851723+SphenHe@users.noreply.github.com>
2024-08-30 18:44:09 +0800

    🐞 fix(generate.sh): remove gpg func
    gpg will generate a signature with time, which makes the signature not reproducible. I'll try another way to realize gpg sign

--- generate.sh
+++ generate.sh
@@ -12,38 +12,24 @@ processBar()
 }
 
 # Read Parameters
-while getopts ":g:s:h" opts; do
+while getopts ":s:h" opts; do
     case ${opts} in
-        g)  gpg_key=$OPTARG
-            # Check if GPG key exists
-            if ! gpg --list-secret-keys $gpg_key > /dev/null 2>&1; then
-                echo "[ERR] GPG Key not found"
-                exit 1
-            else
-                echo "[INF] Using GPG Key: $gpg_key"
-            fi
-            ;;
         s)  secret=$OPTARG
             echo "[INF] Using Secret: $secret"
             ;;
-        h)  echo "[INF] Usage: $0 [-g <gpg_key>] [-s <secret>]"
+        h)  echo "[INF] Usage: $0 [-s <secret>]"
             echo "[INF]     Default: use secret from secret.txt."
-            echo "[INF]     You can also provide a GPG key or a custom secret instead to generate the certificates."
+            echo "[INF]     You can also provide a custom secret instead to generate the certificates."
             exit 1
             ;;
-        \?) echo "[ERR] Invalid option: $OPTARG" 1>&2
+        \?) echo "[ERR] Invalid option: $OPTARG, Use -h for help"
             exit 1
             ;;
     esac
 done
 
-if [ -n "$gpg_key" ] && [ -n "$secret" ]; then
-    echo "[ERR] Both GPG key and secret provided. Please provide only one."
-    exit 1
-fi
-
-if [ -z "$gpg_key" ] && [ -z "$secret" ]; then
-    echo "[ERR] No GPG key or secret provided. Using default key in secret.txt"
+if [ -z "$secret" ]; then
+    echo "[INF] No secret provided. Using default key in secret.txt"
     if [ -f secret.txt ]; then
         secret=$(cat secret.txt)
         if [ -z "$secret" ]; then
@@ -71,13 +57,8 @@ while IFS=',' read -r name prize contest; do
 
     echo "$prize" > prize.typ
 
-    if [ -z "$gpg_key" ]; then
-        combined="${name}${prize}${contest}${secret}"
-        echo -n "$combined" | tr -d '[:space:]' | sha1sum | awk '{print $1}' > fingerprint.typ
-    else
-        combined="${name}${prize}${contest}"
-        echo -n "$combined" | tr -d '[:space:]' | gpg --sign --local-user $gpg_key --armor | sha1sum | awk '{print $1}' > fingerprint.typ
-    fi
+    combined="${name}${prize}${contest}${secret}"
+    echo -n "$combined" | tr -d '[:space:]' | sha1sum | awk '{print $1}' > fingerprint.typ
 
     typst compile template.typ "output/${name}.pdf"
 
